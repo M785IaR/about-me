429e55e05cd14e51a9bc42dd3a9d1685608883d6
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-09-08 19:29:50 +0900

    mark up

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ const swiper = new Swiper(".swiper-container", {
     loop: true, // ループの有効化
     grabCursor: true,
     effect: "cards",
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+    prev: { // 表示しているスライドの移動先
+      shadow: true, // 影の有効化
+      translate: [0, 0, -400], // translateをX,Y,Zで指定
+    },
+    next: { // 次に表示されるスライドの設定
+      translate: ["100%", 0, 0], // translateをX,Y,Zで指定
+    },
+},
 });
 
 })
