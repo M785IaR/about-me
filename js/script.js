b9f2d221cd133a019c5dd87d3ab097e77a44dc3d
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-09-08 16:32:20 +0900

    mark up
    TOPページのjavascriptを付けた

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded',() => {
+const swiper = new Swiper(".swiper-container", {
+    // ドットインジケーターの表示
+    pagination: {
+    el: ".swiper-pagination",
+    },
+  // 前後スライドボタンを表示
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true, // ループの有効化
+    grabCursor: true,
+    effect: "cards",
+});
+})
